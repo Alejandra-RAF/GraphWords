0a85ea6fb4e7e8132f754214b7ba19fa3d5445b9
Alejandra Ruiz de Adana Fleitas <115559347+Alejandra-RAF@users.noreply.github.com>
2025-01-04 23:04:32 +0000

    Update create_api.py

--- src/utility_scripts/create_api.py
+++ src/utility_scripts/create_api.py
@@ -209,5 +209,5 @@ def main(event, context):
             "body": response.get_data(as_text=True)
         }
 
-if __name__ == '__main__':
-    app.run(debug=True)
+#if __name__ == '__main__':
+#    app.run(debug=True)
