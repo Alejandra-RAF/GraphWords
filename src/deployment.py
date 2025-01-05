8b30c97dafb2d36848434b032555262256f79874
Aris Vazdekis Soria <arissoriano71@gmail.com>
2025-01-05 00:03:31 +0000

    Update deployment.py

--- src/deployment.py
+++ src/deployment.py
@@ -4,7 +4,7 @@ import os
 import time
 import json
 
-LOCALSTACK_URL = os.getenv('LOCALSTACK_URL', 'http://localhost:4566')
+LOCALSTACK_URL = os.getenv('LOCALSTACK_URL', 'http://172.17.0.2:4566')
 print(f"Conectando a LocalStack en {LOCALSTACK_URL}")
 
 # Crear clientes para AWS y LocalStack
