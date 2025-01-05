60ef045989743aeb3df28bcf453d045e2ddafe66
Alejandra Ruiz de Adana Fleitas <115559347+Alejandra-RAF@users.noreply.github.com>
2025-01-05 12:39:09 +0000

    Update test_api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -2,6 +2,10 @@ import requests
 import os
 
 def test_api():
+    api_url = os.getenv("API_URL")  # Ahora toma la URL pública expuesta por Ngrok
+    if not api_url:
+        raise ValueError("API_URL no está configurada")
+
     endpoints = [
         "/camino_mas_largo?start=the&end=for",
         "/Dijkstra/?start=the&target=for",
@@ -9,26 +13,16 @@ def test_api():
         "/nodos_alto_grado?umbral=3",
         "/nodos_grado_especifico?grado=2"
     ]
-    
-    # Obtener la URL de la API desde la variable de entorno
-    api_url = os.getenv("API_URL", "http://172.17.0.2:4566")  # Default a localhost si no se configura
-    
-    if not api_url:
-        print("Error: No se ha definido la variable de entorno 'API_URL'.")
-        return
-
-    print(f"Usando API_URL: {api_url}")
 
     for endpoint in endpoints:
-        print(f"\nTesting endpoint: {endpoint}")
+        full_url = f"{api_url}{endpoint}"
+        print(f"\nTesting endpoint: {full_url}")
         try:
-            response = requests.get(f"{api_url}{endpoint}")
+            response = requests.get(full_url)
             print(f"Status Code: {response.status_code}")
-
             try:
-                json_data = response.json()
-                print("Response JSON:", json_data)
-            except requests.exceptions.JSONDecodeError:
+                print("Response JSON:", response.json())
+            except Exception:
                 print("Non-JSON response:", response.text)
 
         except requests.exceptions.RequestException as e:
