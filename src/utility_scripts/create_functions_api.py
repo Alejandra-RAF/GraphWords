eb2aaeef6b5471da34c07fe388cc80b4c135425a
Aris Vazdekis Soria <arissoriano71@gmail.com>
2025-01-05 14:00:08 +0000

    Update create_functions_api.py

--- src/utility_scripts/create_functions_api.py
+++ src/utility_scripts/create_functions_api.py
@@ -116,11 +116,12 @@ def detectar_clusters(graph):
     clusters = []
 
     def dfs(node, cluster):
-        if node not in visited:
-            visited.add(node)
-            cluster.append(node)
-            for neighbor in graph[node]:
-                dfs(neighbor, cluster)
+    if node not in visited:
+        visited.add(node)
+        cluster.append(node)
+        for weight, neighbor in graph[node]:  # Cambia para desempaquetar tupla
+            dfs(neighbor, cluster)
+
 
     for node in graph:
         if node not in visited:
